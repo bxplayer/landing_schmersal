4d37c7948e3bd0c038c21068f683060b3218407b
bxplayer <bxplayer10@gmail.com>
2021-08-10 23:41:56 -0300

    Cambios en html, agregado validacion de js y envio de form

--- js/site.js
+++ js/site.js
@@ -1,26 +1,62 @@
-let email = document.getElementById("email");
+let btn_submit = document.getElementById("btn_submit");
+let form = document.getElementById("form");
 
+let email = document.getElementById("email");
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+let empresa = document.getElementById("empresa");
+let cargo = document.getElementById("cargo");
+let celular = document.getElementById("celular");
+let localidad = document.getElementById("localidad");
+let motivo = document.getElementById("motivo");
+
+
+let IdEvento = document.getElementById("IdEvento");
+let IdSector = document.getElementById("IdSector");
+let IdProfesion = document.getElementById("IdProfesion");
+let IdContactoR2 = document.getElementById("IdContactoR2");
+let Tipo = document.getElementById("Tipo");
+let Oid = document.getElementById("Oid");
+let Chekin = document.getElementById("Chekin");
+let ts = document.getElementById("ts");
 
 
 
 let buscar_mail = () => {
     
 
-    if(email.value.length > 4){
-        let url = 'https://www.dinatecnica.com.ar/schmersal/procesar.php?buscar_email=awrred';
-
-        //let url = 'https://jsonplaceholder.typicode.com/todos/1';
-
-        // var url = new URL('https://sl.se')
-
-        // var params = {lat:35.696233, long:139.570431} // or:
-        // url.search = new URLSearchParams(params).toString();
-       
+    if(email.value.length > 4 && email.value.includes('@')){
+        let url = 'https://www.dinatecnica.com.ar/schmersal/procesar.php?buscar_email=' + email.value;               
         fetch(url,{            
-            mode: 'no-cors'         
+            method: 'GET',        
         })
-        .then((res) => console.log(res))
-        
+        .then((response) => response.json())
+        .then(res => {
+
+            if (res.IdContactoR2>0){
+
+                
+				nombre.value = res.Nombre;				
+				apellido.value = res.Apellido;				
+				empresa.value = res.Empresa;
+				setSelectedValue(cargo, parseInt(res.IdCargo));
+                celular.value = res.Celular;
+                motivo.value = (res.Motivo == null)?0:res.Motivo;
+
+
+				IdSector.value = res.IdSector;                
+				IdProfesion.value = res.IdProfesion;				            
+                IdContactoR2.value = res.IdContactoR2;
+                rubro.value = res.IdRubro;
+				Tipo.value = res.Tipo;
+				Oid.value = res.Oid;
+				Chekin.value = res.Chekin;
+				ts.value = res.ts;
+
+
+            }
+
+        })
         .catch((err) => console.log(err));
 
 
@@ -28,5 +64,36 @@ let buscar_mail = () => {
     
 }
 
-
 email.addEventListener('keyup', buscar_mail);
+
+function setSelectedValue(selectObj, valueToSet) {
+    for (var i = 0; i < selectObj.options.length; i++) {        
+        if (selectObj.options[i].value== valueToSet) {
+            selectObj.options[i].selected = true;
+            return;
+        }
+    }
+}
+
+
+btn_submit.addEventListener('click', () => {
+
+    let submit_form = true;
+    let elemetos_validacion = document.querySelectorAll("input[type=text],input[type=email],select");
+    
+    Array.from(elemetos_validacion).map( x => {        
+        if(x.hasAttribute("required") && (x.value == "" || x.value == "0") ){
+            submit_form = false;
+            x.nextElementSibling.style.display = 'block';            
+            x.nextElementSibling.textContent = `El campo ${x.getAttribute("placeholder")} es requerido.`;
+        }else{
+            x.nextElementSibling.style.display = 'none';
+        }
+
+        if(submit_form)
+        {
+            document.getElementById("form").submit()
+        }        
+    })
+
+})
